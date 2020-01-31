ca37df159ef3a2e9b7af80e0af688cab93c0a57f
Abel Salgado Romero <abelromero@gmail.com>
2020-01-31 12:12:06 +0100

    allow importing data into preexisting MongoDB instance + fix getting collection name as database during import

---
+++ src/main/java/org/abelsromero/embedded/mongo/MongoHandler.java
@@ -0,0 +1,30 @@
+package org.abelsromero.embedded.mongo;
+
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+
+import java.io.IOException;
+
+public class MongoStarter {
+
+    public static MongodExecutable start (String bindIp, int port, String version) throws IOException {
+        final MongodExecutable executable = MongodStarter
+            .getDefaultInstance()
+            .prepare(buildMongoConfig(bindIp, port, version));
+        executable.start();
+        return executable;
+    }
+
+
+    private static  IMongodConfig buildMongoConfig(String bindIp, int port, String version) throws IOException {
+        return new MongodConfigBuilder()
+            .version(Version.valueOf(version))
+            .net(new Net(bindIp, port, Network.localhostIsIPv6()))
+            .build();
+    }
+}
